5d9a26fad0eaad97fe59699354db73be2f5cc28f
Allan Cardoso Araujo <112924458+AllanCardosoDev@users.noreply.github.com>
2025-03-03 22:43:18 -0300

    Update app.py

--- app.py
+++ app.py
@@ -12,15 +12,16 @@ TIPOS_ARQUIVOS_VALIDOS = [
 
 CONFIG_MODELOS = {
     "Groq": {
-    "gema2-9b-isto",
-    "llama-3.3-70b-versátil",
+    "distil-whisper-large-v3-en",
+    "gemma2-9b-it",
+    "llama-3.3-70b-versatile",
     "llama-3.1-8b-instant",
-    "guarda-lhama-3-8b",
-    "lhama3-70b-8192",
-    "lhama3-8b-8192",
+    "llama-guard-3-8b",
+    "llama3-70b-8192",
+    "llama3-8b-8192",
     "mixtral-8x7b-32768",
-    "sussurro-grande-v3",
-    "sussurro-grande-v3-turbo",
+    "whisper-large-v3",
+    "whisper-large-v3-turbo",
         ],
         "chat": ChatGroq,
     },
